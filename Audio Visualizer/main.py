c45e0ffad6b814521efb153827dade65fc14fea2
Killercarrot102 <killercarrot102@outlook.com>
2024-06-07 08:30:41 -0500

    Changed recording into a class and a function

--- Audio Visualizer/main.py
+++ Audio Visualizer/main.py
@@ -1,29 +1,41 @@
-import pyaudio, wave
+import pyaudio
+import wave
+
+
+class recorder(pyaudio.PyAudio):
+    def __init__(self):
+        super().__init__()
+
+    def record(self, seconds, filename) -> wave.Wave_write:
+        """Record a wav file for certain number of seconds. Save with the file name of filename"""
+
+        stream = self.open(format=pyaudio.paInt16, channels=1, rate=44100, input=True, frames_per_buffer=1024)
+
+        frames = []
+
+        for i in range(0, int(44100 / 1024 * seconds)):
+            data = stream.read(1024)
+            frames.append(data)
+
+        # Stop Recording
+        stream.stop_stream()
+        stream.close()
+        self.terminate()
+
+        wave_file = wave.open(filename, 'wb')
+        wave_file.setnchannels(1)
+        wave_file.setsampwidth(self.get_sample_size(pyaudio.paInt16))
+        wave_file.setframerate(44100)
+        wave_file.writeframes(b''.join(frames))
+        wave_file.close()
+
+        return wave_file
 
 
 def main() -> None:
-    recording_len = 10
+    listener = recorder()
 
-    audio = pyaudio.PyAudio()
-    stream = audio.open(format=pyaudio.paInt16, channels=1, rate=44100, input=True, frames_per_buffer=1024)
-
-    frames = []
-
-    for i in range(0, int(44100 / 1024 * recording_len)):
-        data = stream.read(1024)
-        frames.append(data)
-
-    # Stop Recording
-    stream.stop_stream()
-    stream.close()
-    audio.terminate()
-
-    wave_file = wave.open("cool.wav", 'wb')
-    wave_file.setnchannels(1)
-    wave_file.setsampwidth(audio.get_sample_size(pyaudio.paInt16))
-    wave_file.setframerate(44100)
-    wave_file.writeframes(b''.join(frames))
-    wave_file.close()
+    listener.record(10, "new.wav")
 
 
 if __name__ == "__main__":
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Hello!
 
-Welcome to my little Github repo! This has some mini projects that I have made in it. Feel free to add projects (send a pull request) and I might put it in (if I see and like it). If you would like to add a project make sure to add some info on what it does here in the README.md file. Thanks to all that make contributions.
+Welcome to my little GitHub repo! This has some mini projects that I have made in it. Feel free to add projects (send a pull request) and I might put it in (if I see and like it). If you would like to add a project make sure to add some info on what it does here in the README.md file. Thanks to all that make contributions.
 
 Another nice thing to do: <br/>
 I have no skills when it comes to markdown (just look at the source text for this file). Could someone please help me fix this markdown file. Thank you! 
